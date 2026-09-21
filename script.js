9ff88751e8008385742de3dd1c5b08dfcdd42a75
yashprajapati0905-crypto <yashprajapati0905@gmail.com>
2026-09-21 13:46:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+// ===============================
+// MOBILE MENU
+// ===============================
+
+const menuBtn = document.getElementById("menu-btn");
+const nav = document.querySelector("nav");
+
+menuBtn.addEventListener("click", () => {
+
+    nav.classList.toggle("active");
+
+});
+
+
+// ===============================
+// CLOSE MENU AFTER CLICK
+// ===============================
+
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        nav.classList.remove("active");
+
+    });
+
+});
